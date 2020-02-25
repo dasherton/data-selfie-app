433c2bb6e684c90cfaac4bfe3c18eee12e49d955
David Atherton <davidatherton92@gmail.com>
2020-02-25 09:14:35 +0000

    Store coords in simple database

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ app.listen(3000, () => {
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 
+const database = [];
+
 app.post('/api', (request, response) => {
 	console.log(request.body);
+	database.push(request.body);
+	console.log(database);
 
 	response.json({
 		status: 'success',
