750b6ab785d7195cfdce1dc6e5ce5b6c21449a22
David Atherton <davidatherton92@gmail.com>
2020-02-25 09:31:29 +0000

    Implement data persistence using nedb

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Express = require('express');
+const Datastore = require('nedb');
 
 app = Express();
 
@@ -9,11 +10,12 @@ app.listen(3000, () => {
 app.use(Express.static('public'));
 app.use(Express.json({limit: '1mb'}));
 
-const database = [];
+const database = new Datastore('database.db');
+database.loadDatabase();
 
 app.post('/api', (request, response) => {
 	console.log(request.body);
-	database.push(request.body);
+	database.insert(request.body);
 	console.log(database);
 
 	response.json({
